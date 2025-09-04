b41a79f9da1b7d70e4c7b4a72e75463ee966a7e9
valli25 <pravallika.tsm@gmail.com>
2025-09-04 13:06:14 -0700

    adding files

---
+++ Infrastructure-module/locals.tf
@@ -0,0 +1,3 @@
+locals {
+    vpc_id = module.this.vpc_id
+}
--- Infrastructure-module/vpc.tf
+++ Infrastructure-module/vpc.tf
@@ -1,18 +1,21 @@
 module "this" {
-  source = "../Terraform-vpc-module-1"
+  source = "git::https://github.com/valli25/Timing-infra.git?ref=v0.0.2"
   project_name = var.project_name
   vpc_cidr = var.vpc_cidr
   vpc_tags = var.common_tags
   igw_tags = var.common_tags
-  public_subnet_cidr = var. public_subnet_cidr
+  public_subnet_cidr = var.public_subnet_cidr
   public_subnet_tags = var.common_tags
   public_route_table_tags = var.common_tags
+
   private_subnet_cidr = var.private_subnet_cidr
   private_subnet_tags = var.common_tags
   private_route_table_tags = var.common_tags
+
   database_subnet_cidr = var.database_subnet_cidr
   database_route_table_tags = var.common_tags
   database_subnet_tags = var.common_tags
+
   eip_tags = var.common_tags
   nat_gateway_tags = var.common_tags
  
