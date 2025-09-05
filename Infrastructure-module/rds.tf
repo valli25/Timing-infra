685bbdec3eed78f3fdbd28a7f7940e595c896c7d
valli25 <pravallika.tsm@gmail.com>
2025-09-05 08:03:46 -0700

    added another folders

--- Infrastructure-module/rds.tf
+++ Infrastructure-module/rds.tf
@@ -1,13 +1,13 @@
-module "rds" {
-  source = "git::https://github.com/valli25/Timing-infra.git?ref=v0.0.2"
-  allocated_storage = var.allocated_storage
-  db_name = var.db_name
-  engine = var.engine
-  instance_class = var.instance_class
-  rds_username = var.rds_username
-  rds_security_group_ids = [local.rds_Security_group_id]
-  db_subnet_group_name = local.db_subnet_group_name
-  rds_password_arn = var.rds_password_arn
-  tags = var.common_tags
+# module "rds" {
+#   source = "git::https://github.com/valli25/Timing-infra.git?ref=v0.0.2"
+#   allocated_storage = var.allocated_storage
+#   db_name = var.db_name
+#   engine = var.engine
+#   instance_class = var.instance_class
+#   rds_username = var.rds_username
+#   rds_security_group_id = [local.rds_Security_group_id]
+#   db_subnet_group_name = local.db_subnet_group_name
+#   rds_password_arn = var.rds_password_arn
+#   tags = var.common_tags
 
-}
+#}
