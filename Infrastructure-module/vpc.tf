0e70baeb824e389c6b368b854258ebf4252b93bd
valli25 <pravallika.tsm@gmail.com>
2025-09-05 11:06:44 -0700

    added another folders

--- Infrastructure-module/vpc.tf
+++ Infrastructure-module/vpc.tf
@@ -2,20 +2,20 @@ module "this" {
   source = "git::https://github.com/valli25/Timing-infra.git?ref=v0.0.2"
   project_name = var.project_name
   vpc_cidr = var.vpc_cidr
-  vpc_tags = var.common_tags
-  igw_tags = var.common_tags
+  vpc_tags = var.common_atgs
+  igw_tags = var.common_atgs
   public_subnet_cidr = var.public_subnet_cidr
-  public_subnet_tags = var.common_tags
-  public_route_table_tags = var.common_tags
-  eip_tags = var.common_tags
-  nat_gateway_tags = var.common_tags
+  public_subnet_tags = var.common_atgs
+  public_route_table_tags = var.common_atgs
+  eip_tags = var.common_atgs
+  nat_gateway_tags = var.common_atgs
   tags = merge(var.nat_gateway_tags)
   private_subnet_cidr = var.private_subnet_cidr
-  private_subnet_tags = var.common_tags
-  private_route_table_tags = var.common_tags
+  private_subnet_tags = var.common_atgs
+  private_route_table_tags = var.common_atgs
   database_subnet_cidr = var.database_subnet_cidr
-  database_route_table_tags = var.common_tags
-  database_subnet_tags = var.common_tags
+  database_route_table_tags = var.common_atgs
+  database_subnet_tags = var.common_atgs
   db_subnet_group_tags = var.common_tags
 }
 
