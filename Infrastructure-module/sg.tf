857f26f22bc9c0ba5ce6da2e5a196807c6c07529
valli25 <pravallika.tsm@gmail.com>
2025-09-05 10:41:17 -0700

    added another folders

--- Infrastructure-module/sg.tf
+++ Infrastructure-module/sg.tf
@@ -1,4 +1,4 @@
-module "this" {
+module "aws_security_group" {
   source = "git::https://github.com/valli25/Timing-infra.git?ref=v0.0.2"
   security_group_name = var.rds_security_group_name 
   security_group_description = var.rds_security_group_description
