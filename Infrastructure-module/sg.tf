d0667207f3466d3767177c045e6cc24d550f3c8e
valli25 <pravallika.tsm@gmail.com>
2025-09-04 17:34:15 -0700

    added another folders

--- Infrastructure-module/sg.tf
+++ Infrastructure-module/sg.tf
@@ -1,5 +1,5 @@
 module "rds_security_group" {
-  source = "../terraform-aws-securitygroup"
+  source = "git::https://github.com/valli25/Timing-infra.git?ref=v0.0.2"
   security_group_name = var.rds_security_group_name
   security_group_description = var.rds_security_group_description
   vpc_id = local.vpc_id
